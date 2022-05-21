502eabd2a9f62cf73bc67c413fd826f8cd036092
ivan <ivanhfc00@gmail.com>
2022-05-21 15:32:45 -0300

    Finalizada área de depoimentos

--- src/app/components/testimonial/testimonial.component.ts
+++ src/app/components/testimonial/testimonial.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Testimonial } from 'src/app/models/testimonial';
+import { TestimonialsService } from 'src/app/shared/services/testimonials.service';
 @Component({
   selector: 'app-testimonial',
   templateUrl: './testimonial.component.html',
-  styleUrls: ['./testimonial.component.css']
+  styleUrls: ['./testimonial.component.css'],
 })
 export class TestimonialComponent implements OnInit {
 
-  constructor() { }
+  testimonial!: Testimonial; 
+  testimonialsService!: TestimonialsService;
+
+  constructor(_testimonialService: TestimonialsService) {
+    this.testimonialsService = _testimonialService;  
+  }
 
   ngOnInit(): void {
+    this.testimonial = this.testimonialsService.getTestimonial();
   }
 
 }
