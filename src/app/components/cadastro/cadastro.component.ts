cdd60659782370a60cd78dd2e457a1a9ef821397
Ivan C <75918347+00f27x@users.noreply.github.com>
2022-05-22 18:32:14 -0300

    Revert "Signup"

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Adress } from 'src/app/models/adress';
-import { AdressService } from 'src/app/shared/services/adress.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,59 +7,9 @@ import { AdressService } from 'src/app/shared/services/adress.service';
 })
 export class CadastroComponent implements OnInit {
 
-  form!: FormGroup;
-  constructor(
-    private addressService: AdressService,
-    private formBuilder: FormBuilder) { }
-
-  returnedAddress!: Adress;
+  constructor() { }
 
   ngOnInit(): void {
-
-
-    this.form = this.formBuilder.group({
-      nome: ["", Validators.required, Validators.minLength(3)],
-      email: ["", Validators.email],
-      telefone: ["", Validators.required],
-      senha: ["", Validators.required, Validators.minLength(6)],
-      confirmacaoDeSenha: ["", Validators.required,],
-      endereco: this.formBuilder.group({
-        cep: ["",],
-        estado: ["", Validators.required],
-        cidade: ["", Validators.required],
-        logradouro: ["", Validators.required],
-        bairro: ["", Validators.required],
-        numero: ["",],
-        complemento: ["",],
-      }),
-      promoCheck: false,
-    }
-    );
-  }
-
-  verifyCep() {
-    if (this.form.value.endereco.cep.length == 8) {
-
-      this.addressService.setCep(this.form.value.endereco.cep);
-
-      this.addressService.getAdress().subscribe((value: Adress) => {
-        this.returnedAddress = value;
-      });
-
-      this.form.patchValue({
-        endereco:{
-          cidade: this.returnedAddress.localidade,
-          estado: this.returnedAddress.uf,
-          bairro: this.returnedAddress.bairro,
-          logradouro: this.returnedAddress.logradouro
-        }
-      })
-    }
-
-
-  }
-  onSubmit() {
-    console.log("foi");
   }
 
 }
