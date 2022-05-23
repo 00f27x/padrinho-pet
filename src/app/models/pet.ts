d648ab1ae16360d6c54ca6c7f437680689aa6617
ivan <ivanhfc00@gmail.com>
2022-05-23 15:27:24 -0300

    Aprimorada estilização da página de buscas

--- src/app/models/pet.ts
+++ src/app/models/pet.ts
@@ -1,9 +1,9 @@
 export interface Pet {
     name: string;
     gender: string;
-    primary_photo_cropped:{
-     small: string | any;   
-    }; 
+    primary_photo_cropped: {
+        small: string;
+    };
     size: string[];
     description: string;
     species: string;
